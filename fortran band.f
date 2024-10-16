3f7ef9eaba5d59a16fb28fd743de44de2987705c
Rishabh nath <162768501+Rishabh8294@users.noreply.github.com>
2024-10-16 19:33:30 +0530

    Add files via upload
    This is band function model for which I have successfully done the fitting in threeml after wrapping it into python.

---
+++ fortran band.f
@@ -0,0 +1,28 @@
+       SUBROUTINE band(EAR,alp,beta,ebr,k,PHOTAR)
+       IMPLICIT NONE
+      
+       DOUBLE PRECISION EAR,PHOTAR
+
+Cf2py  intent(in) EAR
+Cf2py  intent(in) alp     
+Cf2py  intent(in) wd
+Cf2py  intent(in) ebr
+Cf2py  intent(in) k
+Cf2py  intent(out) PHOTAR   
+
+       DOUBLE PRECISION beta,alp,ebr,fac,k,alp
+       fac = ebr*(alp-beta)
+         
+       IF(EAR.LE.fac) THEN
+             PHOTAR = k*((EAR/100.0d0)**alp)
+             PHOTAR = PHOTAR*DEXP(-EAR/ebr)
+       ELSE    
+             PHOTAR = k*(((alp-beta)*(ebr/100.0d0))**(alp-beta))
+             PHOTAR = PHOTAR*DEXP(-(alp-beta))
+             PHOTAR = PHOTAR*(EAR/100.0d0)**beta
+     
+       ENDIF
+
+       END  
+       
+       
